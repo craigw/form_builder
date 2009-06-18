c2e5e7997aedfe03092dc3cdb1c9872b545f217b
craigw <craig@barkingiguana.com>
2009-06-18 01:56:56 -0700

    Allow setting a custom label.

--- lib/form_builder/labeled_form_builder.rb
+++ lib/form_builder/labeled_form_builder.rb
@@ -1,15 +1,26 @@
 module FormBuilder
   class LabeledFormBuilder < ActionView::Helpers::FormBuilder
-    def label_for(field)
-      %Q(<label for="#{object_name}_#{field}">#{field.to_s.humanize}</label>)
+    def label_for(field, label = nil)
+      label_text = label.blank? ? field.to_s.humanize : label
+      %Q(<label for="#{object_name}_#{field}">#{label_text}</label>)
     end
 
     %W(
       check_box radio_button
     ).each do |form_tag_helper|
       form_tag_helper_with_label = <<-CODE
+      #   Set a custom label by passing :label as an option.
+      #
+      #   <% form_for @model do |f| %>
+      #     <%= f.#{form_tag_helper} :name, :label => "Your name" %>
+      #     ...
+      #   <% end %>
+      #
       def #{form_tag_helper}_with_label(field, *args)
-        %Q(<div class="#{form_tag_helper}">\#{#{form_tag_helper}_without_label(field, *args)}\#{label_for(field)}</div>)
+        options = args.first.kind_of?(Hash) ? args.shift : {}
+        label = options.delete(:label)
+        args.unshift options
+        %Q(<div class="#{form_tag_helper}">\#{#{form_tag_helper}_without_label(field, *args)}\#{label_for(field, label)}</div>)
       end
       alias_method_chain :#{form_tag_helper}, :label
       CODE
@@ -21,8 +32,18 @@ module FormBuilder
       password_field select text_area text_field time_select time_zone_select
     ).each do |form_tag_helper|
       form_tag_helper_with_label = <<-CODE
+      #   Set a custom label by passing :label as an option.
+      #
+      #   <% form_for @model do |f| %>
+      #     <%= f.#{form_tag_helper} :name, :label => "Your name" %>
+      #     ...
+      #   <% end %>
+      #
       def #{form_tag_helper}_with_label(field, *args)
-        %Q(<div class="#{form_tag_helper}">\#{label_for(field)}\#{#{form_tag_helper}_without_label(field, *args)}</div>)
+        options = args.first.kind_of?(Hash) ? args.shift : {}
+        label = options.delete(:label)
+        args.unshift options
+        %Q(<div class="#{form_tag_helper}">\#{label_for(field, label)}\#{#{form_tag_helper}_without_label(field, *args)}</div>)
       end
       alias_method_chain :#{form_tag_helper}, :label
       CODE
